ae3a6ee533ceac79df97c4b02fd0de0e7396bc5a
elop123 <142408289+elop123@users.noreply.github.com>
2023-11-13 12:20:01 +0100

    Console methods

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,3 +11,10 @@ const myBooks = ["The Catcher in the Rye",
     "The Da Vinci Code",
     "The Hunger Games"]
 
+
+console.group('konsole opgave');
+console.log('Jeg er en almindenlig konsol log');
+console.log(myInfo);
+console.warn('jeg er en adversel!');
+console.error('jeg er en fejl!');
+console.table(["The Catcher in the Rye", "To Kill a Mockingbird","1984","Pride and Prejudice","The Great Gatsby","The Hobbit","Harry Potter and the Sorcerer's Stone","The Lord of the Rings","The Da Vinci Code","The Hunger Games"]);
